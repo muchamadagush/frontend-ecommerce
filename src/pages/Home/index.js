c6ae71073c437df79689495c045670f181f0fd5a
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-07-20 00:34:33 +0700

    Responsive home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,4 @@
 import styles from "./home.module.css";
-import Navbar from "../../components/module/Navbar"
 import Card from "../../components/module/Card"
 import { Link, useLocation } from "react-router-dom";
 import axios from "axios"
@@ -7,8 +6,14 @@ import { API_URL } from "../../api/index"
 import Slider from "react-slick";
 import qs from 'query-string'
 import image1 from '../../assets/images/1.png'
-
 import React, { useEffect, useState } from 'react'
+import Navbar from "../../components/module/Navbar/Core";
+import Brand from "../../components/module/Navbar/Brand";
+import Toggler from "../../components/module/Navbar/Toggler";
+import Filter from "../../components/module/Navbar/Filter";
+import NavRight from "../../components/module/Navbar/NavRight";
+import Cart from "../../components/module/Navbar/Cart";
+import Auth from "../../components/module/Navbar/Auth";
 
 const Home = (props) => {
   const [products, setProducts] = useState([])
@@ -54,7 +59,7 @@ const Home = (props) => {
         settings: {
           arrows: false,
           centerMode: true,
-          slidesToShow: 1,
+          slidesToShow: 2,
         }
       }
     ]
@@ -94,83 +99,113 @@ const Home = (props) => {
 
   const handleSearch = () => {
     props.history.push(`/?search=${query}`)
-    window.location.reload();
   }
 
   return (
     <>
-      <Navbar handleInputSearch={handleInputSearch} handleSearch={handleSearch} />
+      <Navbar>
+        <Brand />
+        <Toggler>
+          <Filter
+            handleInputSearch={handleInputSearch}
+            handleSearch={handleSearch} />
+          <NavRight>
+            <Cart />
+            <Auth />
+          </NavRight>
+        </Toggler>
+      </Navbar>
+
       <div className={`container ${styles.marginTopBody}`}>
-        {/* Banner */}
-        <div class="my-slider">
-          <Slider {...bannerSlider}>
-            <div class="img-slider">
-              <img src={image1} alt="" />
+        {q === '' ?
+          <>
+            <div class="my-slider">
+              <Slider {...bannerSlider}>
+                <div class="img-slider">
+                  <img src={image1} alt="" />
+                </div>
+                <div class="img-slider">
+                  <img src={image1} alt="" />
+                </div>
+                <div class="img-slider">
+                  <img src={image1} alt="" />
+                </div>
+                <div class="img-slider">
+                  <img src={image1} alt="" />
+                </div>
+              </Slider>
             </div>
-            <div class="img-slider">
-              <img src={image1} alt="" />
-            </div>
-            <div class="img-slider">
-              <img src={image1} alt="" />
-            </div>
-            <div class="img-slider">
-              <img src={image1} alt="" />
-            </div>
-          </Slider>
-        </div>
 
-        {/* Slider category */}
-        <div class={styles.categoryProduct}>
-          <h3 class={styles.title}>Category</h3>
-          <span class={styles.description}>What are you currently looking for</span>
-          <Slider {...categorySlider}>
-            {categories && categories.map((item, index) => (
-              <div className={styles.imgSlider} key={index}>
-                <img className={styles.sliderImage} src={`${API_URL}files/${item.image}`} alt={item.title} />
-              </div>
-            ))}
-          </Slider>
-        </div>
+            <div class={styles.categoryProduct}>
+              <h3 class={styles.title}>Category</h3>
+              <span class={styles.description}>What are you currently looking for</span>
+              <Slider {...categorySlider}>
+                {/* <div className={styles.imgSlider}> */}
+                {categories && categories.map((item, index) => (
+                  <img key={index} className={styles.sliderImage} src={`${API_URL}files/${item.image}`} alt={item.title} />
+                ))}
+                {/* </div> */}
+              </Slider>
+            </div>
 
-        {/* New Product */}
-        <div className="mt-5">
-          <h3 className={styles.title}>New</h3>
-          <h5 className={styles.description}>You’ve never seen it before!</h5>
-          <div className="row row-cols-2">
-            {products && products.map((product, index) => (
-              <div key={index} className={`col-xs-6 col-sm-4 mt-3 ${styles.colMd}`}>
-                <Link className={styles.link} to={`product/${product.id}`}>
-                  <Card
-                    image={product.mainImage}
-                    title={product.title}
-                    price={'$ ' + product.price}
-                    store="Zalora Cloth"
-                  />
-                </Link>
+            <div className="mt-5">
+              <h3 className={styles.title}>New</h3>
+              <h5 className={styles.description}>You’ve never seen it before!</h5>
+              <div className="row row-cols-2">
+                {products && products.map((product, index) => (
+                  <div key={index} className={`col-xs-6 col-sm-4 mt-3 ${styles.colMd}`}>
+                    <Link className={styles.link} to={`product/${product.id}`}>
+                      <Card
+                        image={product.mainImage}
+                        title={product.title.substring(0, 35)}
+                        price={'Rp ' + product.price}
+                        store="Zalora Cloth"
+                      />
+                    </Link>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
+
+            <div className="mt-5">
+              <h3 className={styles.title}>Popular</h3>
+              <h5 className={styles.description}>Find clothes that are trending recently</h5>
+              <div className="row row-cols-2">
+                {products && products.map((product, index) => (
+                  <div className={`col-xs-6 col-sm-4 mt-3 ${styles.colMd}`}>
+                    <Link className={styles.link} to="product/id" key={index}>
+                      <Card
+                        image={product.mainImage}
+                        title={product.title.substring(0, 35)}
+                        price={'Rp ' + product.price}
+                        store="Zalora Cloth"
+                      />
+                    </Link>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </>
+          :
+          <div className="mt-5">
+            <h3 className={styles.title}>Hasil pencarian untuk "{q}"</h3>
+            <h5 className={styles.description}>Find clothes that are trending recently</h5>
+            <div className="row row-cols-2">
+              {products && products.map((product, index) => (
+                <div className={`col-xs-6 col-sm-4 mt-3 ${styles.colMd}`}>
+                  <Link className={styles.link} to="product/id" key={index}>
+                    <Card
+                      image={product.mainImage}
+                      title={product.title.substring(0, 35)}
+                      price={'$ ' + product.price}
+                      store="Zalora Cloth"
+                    />
+                  </Link>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-
-        {/* Popular product */}
-        <div className="mt-5">
-          <h3 className={styles.title}>Popular</h3>
-          <h5 className={styles.description}>Find clothes that are trending recently</h5>
-          <div className="row row-cols-2">
-            {products && products.map((product, index) => (
-              <div className={`col-xs-6 col-sm-4 mt-3 ${styles.colMd}`}>
-                <Link className={styles.link} to="product/id" key={index}>
-                  <Card
-                    image={product.mainImage}
-                    title={product.title}
-                    price={'$ ' + product.price}
-                    store="Zalora Cloth"
-                  />
-                </Link>
-              </div>
-            ))}
-          </div>
-        </div>
+        }
       </div>
     </>
   )

5fc90610cd0cb1eeac1d9b4f26b12c06d64884e5
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-07-30 19:58:49 +0700

    make route component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from "./pages/Home";
-import Product from "./pages/Product";
-import Carts from "./pages/Carts";
-import Checkout from "./pages/Checkout";
-import Seller from "./pages/Profile/Seller";
-import Login from './pages/auth/Login';
+import Router from "./configs/route";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/product/:id" component={Product} />
-        <Route path="/carts" component={Carts} />
-        <Route path="/checkout" component={Checkout} />
-        <Route exact path="/seller" component={Seller} />
-        <Route path="/seller/update/:id" component={Seller} />
-        <Route path="/seller/products" component={Seller} />
-        <Route path="/seller/orders" component={Seller} />
-        <Route path="/seller/cancel" component={Seller} />
-        <Route path="/seller/add-product" component={Seller} />
-        <Route path="/login" component={Login} />
-      </Switch>
-    </BrowserRouter>
+    <Router />
   );
 }
 
---
+++ src/configs/route/index.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from "../../pages/Home";
+import Product from "../../pages/Product";
+import Carts from "../../pages/Carts";
+import Checkout from "../../pages/Checkout";
+import Seller from "../../pages/Profile/Seller";
+import Login from '../../pages/auth/Login';
+import Register from '../../pages/auth/Register';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/product/:id" component={Product} />
+        <Route path="/carts" component={Carts} />
+        <Route path="/checkout" component={Checkout} />
+        <Route exact path="/seller" component={Seller} />
+        <Route path="/seller/update/:id" component={Seller} />
+        <Route path="/seller/products" component={Seller} />
+        <Route path="/seller/orders" component={Seller} />
+        <Route path="/seller/cancel" component={Seller} />
+        <Route path="/seller/add-product" component={Seller} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Router

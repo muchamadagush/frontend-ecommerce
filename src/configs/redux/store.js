bbdc2ddf8c7d577a1883211e3d1336a4035975ec
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-08-01 19:57:15 +0700

    setting redux dev tools

--- src/configs/redux/store.js
+++ src/configs/redux/store.js
@@ -1,7 +1,9 @@
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 export default store

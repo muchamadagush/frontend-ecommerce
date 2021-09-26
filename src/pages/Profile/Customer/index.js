b1f65bab32f2ab4cf69b2fd281465990a4cc8c50
Muchamad Agus Hermawan <muchamadagush@gmail.com>
2021-09-26 23:55:53 +0700

    make responsive sidebare customer

--- src/pages/Profile/Customer/index.js
+++ src/pages/Profile/Customer/index.js
@@ -1,22 +1,43 @@
 import styles from "./customer.module.css";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Profile from "../../../components/module/Content/Customer/Profile";
-import Nav from "../../../components/module/Navbar";
+import Nav from "../../../components/module/Navbar/NavbarProfile";
 import AsideCustomer from "../../../components/module/AsideCustomer";
 import Shipping from "../../../components/module/Content/Customer/Shipping";
 import Orders from "../../../components/module/Content/Customer/Orders";
+import { useState } from "react";
+import Sidebar from "react-sidebar";
+import { useSelector } from "react-redux";
 
 const Customer = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { modal } = useSelector(state => state.address)
+
+  console.log(modal)
   return (
     <>
-      <Nav />
+      <Nav handleSidebar={() => setSidebarOpen(!sidebarOpen)} />
 
-      <div className="row">
-        <div className={`col-md-3 ${styles.paddingTopBody}`}>
+      <div className={styles.containerSeller}>
+        <div
+          className={`${styles.paddingTopBody} ${styles.left} ${styles.isDesktop}`}
+        >
           <AsideCustomer />
         </div>
 
-        <div className={`col-md-9 ${styles.content}`}>
+        <div
+          className={`${styles.paddingTopBody} ${styles.isMobile}`}
+        >
+          <Sidebar
+            sidebar={<AsideCustomer />}
+            open={sidebarOpen}
+            onSetOpen={() => setSidebarOpen(!sidebarOpen)}
+            styles={{ sidebar: { background: "white" } }}
+          >
+          </Sidebar>
+        </div>
+
+        <div className={`${styles.content} ${styles.right} ${sidebarOpen  !== true ? `${styles.index}` : '' }`}>
           <Switch>
             <Route path='/user/profile' component={Profile} />
             <Route path='/user/address' component={Shipping} />
